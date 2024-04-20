d1363bbf7c7bdac7c0fa1033b2438265f68b42a1
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-04-20 11:07:17 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
 #!/usr/bin/pup
-# Install flask from pip3.
-# Requirements:
-#   Install flask
-#   Version must be 2.1.0
-
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
