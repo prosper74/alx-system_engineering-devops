f6275f2ec2d586513d1183387c934082b06dad42
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-06-05 23:38:06 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-fixes typo in /var/www/html/wp-settings
+# fixes typo in /var/www/html/wp-settings
 
 exec { 'fix typo':
 command  => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php; service apache2 restart',
